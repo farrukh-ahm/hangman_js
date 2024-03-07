93f0171adffb0c59496d38bfa15f08a369d72b54
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2024-03-07 22:00:56 +0000

    Customised the page app styles. Add logic for letter guesses and display the result

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-import words from "./words.js"
+import {words, hangmanStage} from "./words.js"
 
 const startButton = document.querySelector(".start-button")
 const categoryBtn = document.querySelectorAll(".cat")
 const gameContainer = document.querySelector(".game-container")
 const wordGuess = document.querySelector(".guess-word")
+const guessButtons = document.querySelectorAll(".selection-btn")
+const hangmanFigure = document.querySelector(".hangman-figure")
+
 const alphas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let selectedWords = []
+let randomWordList
+let turns = 0
 
+
+// ----------- INITIALISE THE GAME SCREEN --------------------------
 const gameSetup = category => {
 
     let cat = category. toLowerCase()
@@ -14,8 +21,10 @@ const gameSetup = category => {
     let randomWord = words[cat][Math.floor(Math.random() * words[cat].length)].toLowerCase()
     console.log(randomWord)
 
+    randomWordList = randomWord.split("")
+    console.log(randomWordList)
+
     for(let i of randomWord){
-        console.log(i)
         if(!alphas.includes(i)){
             
             selectedWords.push("-")
@@ -24,19 +33,47 @@ const gameSetup = category => {
     }
 
     wordGuess.innerText = `${selectedWords.join(" ")}`
+    hangmanFigure.src = `./assets/${hangmanStage[turns]}`
     gameContainer.scrollBy(gameContainer.offsetWidth, 0)
-    console.log(randomWord)
+    // console.log(randomWord)
+
+}
+
+
+// ----------- FXN TO HANDLE PLAYER GUESS -------------------------- 
+const playerGuess = e => {
+
+    let guessedLetter = e.target.innerText.toLowerCase()
+    e.target.disabled = true;
+    if(randomWordList.includes(guessedLetter)){
+        e.target.style.backgroundColor = "#77FC7F";
+
+        for(let i=0; i<randomWordList.length; i++){
+            if(randomWordList[i]===guessedLetter){
+                selectedWords[i] = guessedLetter
+                wordGuess.innerText = `${selectedWords.join(" ").toUpperCase()}`
+            }
+        }
+    }
+    else{
+        e.target.style.backgroundColor = "#FF492B";
+        turns++
+        hangmanFigure.src = `./assets/${hangmanStage[turns]}`
+    }
 
 }
 
 
 
+
+// ----------- START SCREEN BUTTON --------------------------
 startButton.addEventListener("click", ()=>{
     console.log(gameContainer.offsetWidth)
     gameContainer.scrollBy(gameContainer.offsetWidth,0)
 })
 
 
+// ----------- CATEGORY SELECTION --------------------------
 categoryBtn.forEach(catBtn => {
 
     catBtn.addEventListener("click", e =>{
@@ -44,6 +81,19 @@ categoryBtn.forEach(catBtn => {
         let category = e.target.innerText;
         gameSetup(category)
 
+    })
+
+})
+
+
+// ----------- LETTER BUTTONS - Taking User Guesses --------------------------
+guessButtons.forEach(guess => {
+
+    guess.addEventListener("click", (e)=>{
+
+        playerGuess(e)
+        // let guessedLetter = e.target.innerText
+        // console.log(guessedLetter)
     })
 
 })
--- words.js
+++ words.js
@@ -1,4 +1,4 @@
-const words = {
+export const words = {
 
     cities: ["New York", "Los Angeles", "Palestine", "New Delhi", "Canberra", "Cairo", "Galway"],
     animals: ["Elephant", "Leopard", "Kangaroo", "Panda", "Bat", "Koala", "Gorilla", "Anaconda"],
@@ -8,4 +8,6 @@ const words = {
 
 }
 
-export default words
+export const hangmanStage = ["hangman-0.svg", "hangman-1.svg", "hangman-2.svg", "hangman-3.svg", "hangman-4.svg", "hangman-5.svg", "hangman-6.svg"]
+
+// export default words
